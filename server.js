20c07dd896d249231f48001d036447c6944d7fc0
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-08-08 00:01:39 -0300

    .

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const { resolve } = require('path')
+const { resolve, join } = require('path')
 
 const app = express()
 
 app.use('/', express.static(resolve(__dirname, './build')))
+
+app.get('/', function (req, res) {
+  res.sendFile(join(__dirname, './build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) { return console.log(err) }
