81d734f7a9d7f5dfdf8d5ab11d281c800872633b
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-08-08 09:27:44 -0300

    .

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const { resolve, join } = require('path')
+const { resolve } = require('path')
 
 const app = express()
 
-app.use('/', express.static(resolve(__dirname, './build')))
+app.use('/', express.static(resolve(__dirname, 'build')))
 
-app.get('/', function (req, res) {
-  res.sendFile(join(__dirname, './build', 'index.html'));
+app.get('https://web-rickotech.herokuapp.com/', function (req, res) {
+  res.sendFile(resolve(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000, (err) => {
