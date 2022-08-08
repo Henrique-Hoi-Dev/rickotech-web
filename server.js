56fa00fb188ac46bdfe8e36e042d2f16e2a231f6
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2022-08-08 10:28:45 -0300

    .

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express()
 app.use('/', express.static(resolve(__dirname, 'build')))
 
 app.get('/', function (req, res) {
+  console.log(res)
   res.sendFile(resolve(__dirname, 'build', 'index.html'));
+});
+
+app.get("/dashboard", function(req, res){
+  res.render("dashboard");
 });
 
 app.listen(process.env.PORT, (err) => {
